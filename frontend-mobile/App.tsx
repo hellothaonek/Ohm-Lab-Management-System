2ef69de5a4aa87d7a610d207b92fb5c45875f408
isYenthao <thaophy1507@gmail.com>
2025-05-21 09:34:32 +0700

    push mobile setup

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-2xl bg-red-500 font-bold mb-4">Home Screen</Text>
       <StatusBar style="auto" />
     </View>
   );
